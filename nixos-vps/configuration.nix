60290dd6e94c4205ee18a3948c7367dee8580925
moonpiedumplings <jeff.fonseca777@gmail.com>
2023-07-09 14:23:07 -0700

    added openssh public key

--- nixos-vps/configuration.nix
+++ nixos-vps/configuration.nix
@@ -47,13 +47,13 @@
   # };
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.root.initialHashedPassword = "$y$j9T$Z8cnbLUJSR3qeWuC2b6YS/$uz1pC.ArrehUtkHQHebbaglWUxAmlPGADZjT0EnXzQ5";
+  #users.users.root.initialHashedPassword = "$y$j9T$Z8cnbLUJSR3qeWuC2b6YS/$uz1pC.ArrehUtkHQHebbaglWUxAmlPGADZjT0EnXzQ5";
   users.users.moonpie = {
      isNormalUser = true;
      extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
      #packages = with pkgs; [];
      initialHashedPassword = "$y$j9T$ZGDLrUl6VP4AiusK96/tx0$1Xb1z61RhXYR8lDlFmJkdS8zyzTnaHL6ArNQBBrEAm0"; # may replace this with a proper secret management scheme later
-     
+     openssh.authorizedKeys.keys = [ "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDEQDNqf12xeaYzyBFrOM2R99ciY9i0fPMdb4J64XpU3Tjv7Z5WrYx+LSLCVolzKktTfaSgaIDN8+vswixtjaAXkGW2glvTD0IwaO0N4IQloov3cLZa84I7lkj5jIkZiXJ2zHJZ8bQmayUrSj2yBwYJ61QLs+R0hpEZHfRarBU9vphykACdM6wxSj0YVkFnGlKBxZOZipW6OoKjEkFOHOSH6DYrX3V/TqALYC62iH6jEiLIycxey1vfwkywfsP9V9GlGYHutdwgAgeaN3xUnL8+X6vkQ8cbC2jEuVopodsAAArFYZLVdfAcNc17WYq5y+FX3schGpTo89SZ4Uh9gd4b45h9Hq7h6p7hBF8UCkyqSKnFiPjDJxv5yuY+rYeZ9aJSeCJUYrb1xyOreWnJkhDuYff/1NCewWL8sfuD9IC9BXWBwhxoA/OUfV9KvDBZmYoThlh86ZCQ+uqCR1DIKa1YhPMlT6gzUY01yoMj+B93RpUBUW5LqLDVCL7Qujh/0ns= moonpie@cachyos-x8664" ];
    };
 
   # List packages installed in system profile. To search, run:
@@ -82,6 +82,7 @@
         PasswordAuthentication = false;
         PermitRootLogin = "no";
       };
+      openFirewall = true;
     };
     cockpit = {
       enable = true;
